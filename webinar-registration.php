1292d9f88534e59771f8774c5cc2d3dbeac379e0
kshitijbhatt <kbhatt14king@gmail.com>
2019-07-24 15:53:35 +0530

    adding webinar registration page

---
+++ webinar-registration.php
@@ -0,0 +1,38 @@
+<?php include("header.php"); ?>
+<div id="main" class="container align-content-center py-5">
+<div class="row">
+<div class="card-group">
+    <div class="card mr-3 border-0 shadow-none">
+        <div class="card py-2">
+                <div class="card-header bg-white">
+                    <h5 class="card-title"> Register for webinar</h5>
+                </div>
+                <div class="card-body">
+                <form action="https://app4.easysendy.com/lists/hm6266kg3x081/subscribe" accept-charset="utf-8" class="form" role="form" autocomplete="off" id="formRegistration" novalidate="" method="POST">
+                                    <div class="form-group">
+                                        <label for="fname">Full Name</label>
+                                        <input type="text" class="form-control form-control-lg rounded-0" name="FULLNAME" id="fullName" required="">
+                                        <div class="invalid-feedback">Oops, you missed this one.</div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="email" class="form-control form-control-lg rounded-0" id="EMAIL" required="" autocomplete="new-email" name="EMAIL">
+                                        <div class="invalid-feedback">Enter your correct email address</div>
+                                    </div>
+                                    <button type="submit" class="btn text-light bg-avc border-0 btn-lg w-100" id="btnLogin">Register</button>
+                    </form>
+                </div>
+            
+        </div>
+    </div>
+    <div class="col-6 ml-3 border-0 shadow-none">
+        <div class="mx-2 py-2">
+            <p>
+            Auroville Consulting (AVC) invites you to a webinar on our recent report titled Water-Energy-Land-Livelihood Nexus. The agricultural sector in the country is distressed, water stress being a major reason. The agrarian distress is also intertwined with the situation of the power sector. Agricultural supply in the state of Tamil Nadu is free of cost, and presents a major subsidy burden on the state. Solving this water-energy nexus will required a holistic look at it. In our report, we analyse the benefits from such an approach for all the stakeholders: water security and better livelihood for farmers, easing of the massive financial burden on the state and the electricity utility, and benefits to the public at large through job creation and lower emissions. The approach considered consists of utilising the combined benefits of grid-connected solar PV at farms, energy efficient pumps and advanced irrigation (micro and precision irrigation).
+            </p>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<?php include("footer.php");?>
